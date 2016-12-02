8dc5609093417c1be7cdcabdb4c628bafa046fe9
Andrej Krištofič <andrej.kristofic@softec.sk>
2016-12-02 17:24:45 +0100

    Allow subresource locator params to be overriden by params defined in subresource itself

--- src/main/java/com/sebastian_daschner/jaxrs_analyzer/analysis/results/ResultInterpreter.java
+++ src/main/java/com/sebastian_daschner/jaxrs_analyzer/analysis/results/ResultInterpreter.java
@@ -117,12 +117,13 @@ public class ResultInterpreter {
     private Set<MethodParameter> collectMethodParameters(final MethodResult methodResult, final ClassResult classResult) {
 	    Set<MethodParameter> methodParameters = new HashSet<>();
 
-	    updateMethodParameters(methodParameters, classResult.getClassFields());
-	    updateMethodParameters(methodParameters, methodResult.getMethodParameters());
 	    // add parent resource parameters if this is subresource
 	    if (classResult.getParentSubResourceLocator() != null) {
 		    methodParameters.addAll(collectMethodParameters(classResult.getParentSubResourceLocator(), classResult.getParentSubResourceLocator().getParentResource()));
 	    }
+	    updateMethodParameters(methodParameters, classResult.getClassFields());
+	    updateMethodParameters(methodParameters, methodResult.getMethodParameters());
+
 	    stringParameterResolver.replaceParametersTypes(methodParameters);
 	    return methodParameters;
     }
